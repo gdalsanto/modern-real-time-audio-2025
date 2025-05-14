7b2ad0680fc71813b2edf154d1171eb9f9e26576
Gloria Dal Santo <gloria.dalsanto@outlook.it>
2025-05-14 09:16:28 +0300

    comments

--- modules/mrta_utils/Source/GUI/GenericParameterEditor.h
+++ modules/mrta_utils/Source/GUI/GenericParameterEditor.h
@@ -2,13 +2,14 @@
 
 namespace mrta
 {
-
+// list of all parameters created in the audio processor side
+// and will turn them into GUI components (knobs with labels)
 class GenericParameterEditor : public juce::Component
 {
 public:
     GenericParameterEditor(mrta::ParameterManager& parameterManager,
-                           int parameterWidgetHeight = 80,
-                           const juce::StringArray& parameterIDs = {});
+                           int parameterWidgetHeight = 80,  // default height for the parameter widgets in pixels
+                           const juce::StringArray& parameterIDs = {}); // list of parameters IDs of those that you want ot have in the component
     GenericParameterEditor() = delete;
 
     void paint(juce::Graphics&) override;
